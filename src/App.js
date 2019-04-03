37b1a21a43ba4be3e5c9f96879a5eb62140aa78b
github username <github email address>
2019-04-03 14:57:35 +0100

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import { withRouter, Router, Route } from "react-router-dom";
 import SignupLoginContainer from "./container/signupLoginContainer";
 import HomeContainer from "./container/homeContainer";
 import ExploreContainer from "./container/exploreContainer";
@@ -10,9 +11,61 @@ import UserProfileContainer from "./container/userProfileContainer";
 import FormulaFormContainer from "./container/formulaFormContainer";
 
 class App extends Component {
+  state = {};
+
+  routing = () => {
+    return (
+      <div>
+        <Route exact path={`/`} component={() => <HomeContainer />} />
+        <Route
+          exact
+          path={`/login`}
+          component={() => <SignupLoginContainer />}
+        />
+
+        <Route
+          exact
+          path={`/formulas`}
+          component={() => <ExploreContainer />}
+        />
+
+        <Route
+          exact
+          path={`/ingredients`}
+          component={() => <ExploreContainer />}
+        />
+
+        <Route
+          exact
+          path={`/favourites`}
+          component={() => <ExploreContainer />}
+        />
+
+        <Route
+          exact
+          path={`/formulas/create`}
+          component={() => <FormulaFormContainer />}
+        />
+
+        <Route
+          exact
+          path={`/profile`}
+          component={() => <UserProfileContainer />}
+        />
+      </div>
+    );
+  };
+
   render() {
-    return <div className="App">APP</div>;
+    return (
+      <div className="App">
+        <div className="nav_bar">
+          <MainNavContainer />{" "}
+        </div>
+        {this.routing()}
+      </div>
+    );
   }
 }
 
-export default App;
+export default withRouter(App);
--- src/container/MainNavContainer.jsx
+++ src/container/MainNavContainer.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 // app > main nav
 class MainNavContainer extends Component {
   state = {};
+
   render() {
-    return <div>NAV BAR</div>;
+    return (
+      <div>
+        <NavLink to="/"> Home </NavLink>
+        <NavLink to="/formulas"> Formulas </NavLink>
+        <NavLink to="/ingredients"> Ingredients </NavLink>
+        <NavLink to="/favourites"> Favourites </NavLink>
+        <NavLink to="/formulas/create"> Create </NavLink>
+        <NavLink to="/login"> Login </NavLink>
+        <NavLink to="/profile"> Profile </NavLink>
+      </div>
+    );
   }
 }
 
