3435b41c057545e7df764859d03c245c916e6f7b
github username <github email address>
2019-04-15 15:44:36 +0100

    formula can be converted to different units

--- src/presentational/formulaDetails.jsx
+++ src/presentational/formulaDetails.jsx
@@ -1,14 +1,53 @@
 import React, { Component } from "react";
-import { Card, Image, Label, Grid } from "semantic-ui-react";
+import {
+  Card,
+  Image,
+  Label,
+  Grid,
+  Dropdown,
+  Input,
+  Select,
+  Popup
+} from "semantic-ui-react";
 
 // app > formula details container > forumla details
 const tagColours = ["tan", "plantation", "sepiaSkin", "oxley"];
 
+const units = [
+  { key: "ounces", text: "oz", value: "oz" },
+  { key: "grams", text: "g", value: "g" },
+  { key: "mililiters", text: "ml", value: "ml" }
+];
+
 class FormulaDetails extends Component {
-  state = {};
+  state = {
+    unit: "",
+    formulaTotal: 0
+  };
 
   colour = () => {
     return tagColours[Math.floor(Math.random() * tagColours.length)];
+  };
+
+  // ============================================== formula qty functions ==========================================================
+
+  handleFormulaQty = event => {
+    this.setState({
+      formulaTotal: parseInt(event.target.value)
+    });
+  };
+
+  handleUnits = (event, data) => {
+    console.log(data);
+    this.setState({
+      unit: data.value
+    });
+  };
+
+  calculateFormula = percent => {
+    const total = this.state.formulaTotal;
+    const qty = (percent / 100) * total;
+    return qty;
   };
 
   render() {
@@ -56,10 +95,50 @@ class FormulaDetails extends Component {
         <Grid.Row className="detail_info">
           <Grid.Column className="formula_ingredients" width={4}>
             <h2>Ingredients</h2>
-
+            <div className="formula_qty">
+              <Popup
+                trigger={
+                  <Input
+                    label={
+                      <Dropdown
+                        onChange={this.handleUnits}
+                        placeholder="Units"
+                        defaultValue=""
+                        options={units}
+                      />
+                    }
+                    labelPosition="right"
+                    placeholder="total quantity"
+                    onChange={this.handleFormulaQty}
+                  />
+                }
+                content="Calculate formula ingredients based on the total amount you want to make"
+                size="large"
+              />
+            </div>
+            {/* <div className="formula_qty">
+              {/* <label>
+                {" "}
+                Convert ingredient list to grams by entering the formula total
+                and grams bellow{" "}
+              </label> */}
+            {/* <Input
+                label={
+                  <Dropdown
+                    onChange={this.handleUnits}
+                    defaultValue="%"
+                    options={units}
+                  />
+                }
+                labelPosition="right"
+                placeholder="total quantity"
+                onChange={this.handleFormulaQty}
+              />
+            </div> */}
             {ingredients.map(ingredient => (
               <p>
-                •{ingredient.name} - {ingredient.percentage}%
+                •{ingredient.name} - <i>{ingredient.percentage}%</i> ...{" "}
+                {this.calculateFormula(ingredient.percentage)} {this.state.unit}
               </p>
             ))}
           </Grid.Column>
@@ -75,37 +154,3 @@ class FormulaDetails extends Component {
 }
 
 export default FormulaDetails;
-
-{
-  /* <Container className="formula_details_wrapper">
-  <div className="formula_header">
-    <img src={image} alt={title} />
-    <h1>{title}</h1>
-    <h4>
-      By:{user_id} {area} {category}
-    </h4>
-    <p>{description}</p>
-    {concerns.map(concern => (
-      <div className="concern-tag">
-        <p>•{concern.name}</p>
-      </div>
-    ))}
-  </div>
-
-  <div className="formula_ingredients">
-    <h2>Ingredients</h2>
-    <ul>
-      {ingredients.map(ingredient => (
-        <li>
-          {ingredient.name} - {ingredient.percentage}%
-        </li>
-      ))}
-    </ul>
-  </div>
-
-  <div className="formula_directions">
-    <h2>Directions</h2>
-    <p>{directions}</p>
-  </div>
-</Container>; */
-}
